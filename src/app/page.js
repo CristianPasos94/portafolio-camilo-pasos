393ae95d8b132f27fe5f9471d7809fb71c08a7fb
CarolinaChavezDavid <scchavezd@gmail.com>
2023-07-22 17:53:53 -0500

    project creation

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 style={{ color: "#F11A7B"}}>cristian camilo pasos david</h1> 
+      <h2>este es mi primer trabajo</h2>
+      <p>aca pienso mostrar mi primer trabajo donde quiero aprender a desarrollar y crear paginas web y muchas cosas mas </p>
+      <b>negrita</b> 
+    </main>
+  )
+}
